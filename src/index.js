26671d0c60c7fccdad1d4025890ab29871c314a9
mikeokol <225904631+mikeokol@users.noreply.github.com>
2026-03-14 06:22:57 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SpiritCoach from './SpiritCoach';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SpiritCoach />
+  </React.StrictMode>
+);
